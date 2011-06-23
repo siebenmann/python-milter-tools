202173d61e5923267bd199f7f743a555f3f25ce0
Chris Siebenmann <cks.git01@cs.toronto.edu>
2011-06-23 17:03:11 -0400

    __init__.py: neurotically re-wrap one documentation line to be shorter

--- milter/__init__.py
+++ milter/__init__.py
@@ -3,8 +3,8 @@
 # make it import everything useful.
 __doc__ = """A collection of tools for dealing with the sendmail milter protocol.
 
-Useful submodules are milter.codec and milter.convo. See their documentation
-for details."""
+Useful submodules are milter.codec and milter.convo. See their
+documentation for details."""
 
 from consts import *
 import codec
